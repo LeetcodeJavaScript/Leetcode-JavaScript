44f450611166bca39103a758af2ab95922bc3fdc
LeetcodeJavaScript <LeetcodeJavaScript@outlook.com>
2016-05-29 23:00:36 -0700

    Update 235.js

--- 235.js
+++ 235.js
@@ -1,11 +1,14 @@
 function lowestCommonAncestor(root, p, q) {
     var min = Math.min(p.val, q.val);
     var max = Math.max(p.val, q.val);
+    
     if (root.val > max) {
         return lowestCommonAncestor(root.left, p, q);
     }
+    
     if (root.val < min) {
         return lowestCommonAncestor(root.right, p, q);
     }
+    
     return root;
 }

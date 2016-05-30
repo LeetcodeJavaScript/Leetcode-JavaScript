6c543350164bde920ebc182a8ee3df6b65dcef12
LeetcodeJavaScript <LeetcodeJavaScript@outlook.com>
2016-05-29 21:23:51 -0700

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,13 @@
+function twoSum(nums, target) {
+    var map = {};
+    for (var i = 0; i < nums.length; i++) {
+        var curr = nums[i];
+        var rest = target - curr;
+        
+        if (map[rest] !== undefined) {
+            return [map[rest], i];
+        }
+        
+        map[curr] = i;
+    }
+}

e180d4b0bba1dbc7f649e18bbb3c7ab01af65fa9
LeetcodeJavaScript <LeetcodeJavaScript@outlook.com>
2016-05-29 22:55:51 -0700

    Update 206.js

--- 206.js
+++ 206.js
@@ -1,10 +1,12 @@
 function reverseList(head) {
     var prev = null;
+    
     while (head) {
         var next = head.next;
         head.next = prev;
         prev = head;
         head = next;
     }
+    
     return prev;
 }

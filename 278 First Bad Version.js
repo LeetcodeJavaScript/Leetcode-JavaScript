45eb710b8957cc6aebdb4f8a07c67b0ff31106df
LeetcodeJavaScript <LeetcodeJavaScript@outlook.com>
2016-06-19 09:05:07 -0700

    Update 278 First Bad Version.js

--- 278 First Bad Version.js
+++ 278 First Bad Version.js
@@ -5,20 +5,35 @@ function solution(isBadVersion) {
         var l = 0;
         var r = n;
         
+        // Try to make l and r as the index of first bad version
+        // so if mid is bad verion
+        // FROM [G (L)] G G G G B B [B (M)] B B [B (R)]
+        // TO   [G (L)] G G G G B B [B (R)]
+        // 
+        // but if mid is good version
+        // FROM [G (L)] G [G (M)] G G B B B B B [B (R)]
+        // TO               [G (L)] G B B B B B [B (R)]
+        
+        
+        // loop should terminates when l === r
         while (l < r) {
             var mid = Math.floor((l + r) / 2);
         
             if (isBadVersion(mid)) {
                 // r never equals m
                 // so there won't be infinite loop
+                
+                // keep r in bad version
                 r = mid;
             } else {
+                
+                // try to move l to bad version
                 l = mid + 1;
             }
         }
         
-        // postcondition
-        // l === r as the first bad version
+        // post loop condition
+        // l === r as the index of first bad version
         return l;
     }
 }
